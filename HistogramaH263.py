27375e30f28ec9ca66237d97dc8a491472bff112
AdrianLCS <adrian.lima.02.01@gmail.com>
2022-10-28 08:24:57 -0300

    final

--- HistogramaH263.py
+++ HistogramaH263.py
@@ -10,7 +10,7 @@ for i in vline:
 
 m = 0
 var = 0
-freq = 1 / len(l)
+freq = 1/ len(l)
 
 for i in l:
     m = m + i * freq
@@ -18,6 +18,15 @@ print(m)
 
 for i in l:
     var = var + ((i - m) ** 2) * freq
+g = []
+i = 0
+h = 100
+while i < len(l)-h:
+    s = 0
+    for j in range(h):
+       s += l[i+j]
+    g.append(s)
+    i += h
 
 print(var)
 desvpad = var ** (1 / 2)
@@ -26,8 +35,10 @@ print(desvpad)
 
 l = np.array(l)
 
-plt.title('Histograma: mr_bean_h263_64k \n Média: 1115.5629311012997 \n Variância: 197099.38507710805 \n Desvio padrão: 443.9587650639506')
-plt.xlabel('valor')
-plt.ylabel('Frequência Relativa')
-plt.hist(l, 100, rwidth=0.95, weights=np.ones_like(l) / len(l))
+#plt.title('Histograma: mr_bean_h263_64k \n Média: 1115.5629311012997 \n Variância: 197099.38507710805 \n Desvio padrão: 443.9587650639506')
+#plt.xlabel('valor')
+#plt.ylabel('Frequência Relativa')
+
+plt.hist(g, rwidth=0.98, weights=np.ones_like(g) / len(g))
+plt.hist(g, 25)
 plt.show()
